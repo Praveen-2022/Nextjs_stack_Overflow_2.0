b37b68ca608cd9a5836f126aa3bccb0901460338
Praveen Kumar Chauhan <praveenkumarchauhan2022@gmail.com>
2023-11-29 16:18:06 +0530

    Navbar done

--- context/ThemeProvider.tsx
+++ context/ThemeProvider.tsx
@@ -2,22 +2,26 @@
 
 import React, { createContext, useContext, useState, useEffect } from "react";
 
-interface ThemeContextType{
-    mode:string;
-    setMode:(mode:string)=> void;
+interface ThemeContextType {
+  mode: string;
+  setMode: (mode: string) => void;
 }
-const ThemeContext = createContext<ThemeContextType | undefined >(undefined);
+const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
 
 export function ThemeProvider({ children }: { children: React.ReactNode }) {
   const [mode, setMode] = useState("");
 
   const handleThemeChange = () => {
-    if (mode === "dark") {
-      setMode("light");
-      document.documentElement.classList.add("light");
-    } else {
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme:dark)").matches)
+    ) {
       setMode("dark");
       document.documentElement.classList.add("dark");
+    } else {
+      setMode("light");
+      document.documentElement.classList.remove("dark");
     }
   };
 
