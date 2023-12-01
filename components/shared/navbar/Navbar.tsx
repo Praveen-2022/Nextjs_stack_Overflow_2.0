c954d03dd8ea97c4b3a0f4a63cff61c42267f986
Praveen Kumar Chauhan <praveenkumarchauhan2022@gmail.com>
2023-12-01 12:08:52 +0530

    Navbar done

--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+
 import Link from "next/link";
 import Image from "next/image";
 import { SignedIn, UserButton } from "@clerk/nextjs";
@@ -8,19 +9,22 @@ import GlobalSearch from "../search/GlobalSearch";
 
 const Navbar = () => {
   return (
-    <nav className="flex-between bg-light-900 dark:bg-dark-200 fiexd z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
+    <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
       <Link href="/" className="flex items-center gap-1">
         <Image
           src="/assets/images/site-logo.svg"
           width={23}
           height={23}
-          alt="DevFlow Logo"
+          alt="DevFlow"
         />
-        <p className="text-[24px] font-bold leading-[31.2px] font-spaceGrotesk text-dark-100 dark:text-light-900 max-sm:hidden">
-          Dev <span className="text-primary-500">Overflow</span>
+
+        <p className="h2-bold font-spaceGrotesk text-dark-100 dark:text-light-900 max-sm:hidden">
+          Dev <span className="text-primary-500">OverFlow</span>
         </p>
       </Link>
-      <GlobalSearch/>
+
+      <GlobalSearch />
+      
       <div className="flex-between gap-5">
         <Theme />
         <SignedIn>
