7c1516b113c020c75ba872318a61e79987f2ca5c
Praveen Kumar Chauhan <praveenkumarchauhan2022@gmail.com>
2023-12-16 14:42:00 +0530

    Refactor:Done

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -25,11 +25,12 @@ export async function getUserById(params: any) {
     connectToDatabase();
 
     const { userId } = params;
+
     const user = await User.findOne({ clerkId: userId });
+
     return user;
   } catch (error) {
     console.log(error);
-    throw error;
   }
 }
 
@@ -72,11 +73,7 @@ export async function deleteUser(params: DeleteUserParams) {
     if (!user) {
       throw new Error("User not found!");
     }
-    // Delete User from Database
-    // and questions,answers,comments,etc.
-    // const userQuestionIds = await Question.find({ author: user._id }).distinct(
-    //   "_id"
-    // );
+
     // Delete User Questions
     // await Question.deleteMany({ author: user._id });
 
@@ -311,6 +308,44 @@ export async function getUserInfo(params: GetUserByIdParams) {
       totalAnswers,
       badgeCounts,
     };
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function getUserQuestions(params: GetUserStatsParams) {
+  try {
+    connectToDatabase();
+
+    const { userId } = params;
+
+    const totalQuestions = await Question.countDocuments({ author: userId });
+    const userQuestions = await Question.find({ author: userId })
+      .sort({ createdAt: -1, views: -1, upvotes: -1 })
+      .populate("tags", "_id name")
+      .populate("author", "_id clerkId name picture");
+
+    return { totalQuestions, questions: userQuestions };
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function getUserAnswers(params: GetUserStatsParams) {
+  try {
+    connectToDatabase();
+
+    const { userId } = params;
+
+    const totalAnswers = await Answer.countDocuments({ author: userId });
+    const userAnswers = await Answer.find({ author: userId })
+      .sort({ upvotes: -1 })
+      .populate("question", "_id title")
+      .populate("author", "_id clerkId name picture");
+
+    return { totalAnswers, answers: userAnswers };
   } catch (error) {
     console.log(error);
     throw error;
