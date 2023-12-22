75c09d4a30f0bf21ee33c40e28b164dc6ed07790
Praveen Kumar Chauhan <praveenkumarchauhan2022@gmail.com>
2023-12-22 15:04:57 +0530

    refactor: added jobs

---
+++ app/(root)/jobs/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Jobs = () => {
+  return (
+    <>
+      <h1 className="h1-bold text-dark100_light900">Jobs</h1>
+      <p className="body-medium text-dark500_light700">
+        Added Soon....
+      </p>
+    </>
+  );
+};
+
+export default Jobs;
